73f59669bd5fc1bbf2de1f2e9e0f4e36f2903ec1
WodaMineralna <raknieborak65@gmail.com>
2025-04-16 20:36:55 +0200

    userPlaces data updating logic to backend

--- src/http.js
+++ src/http.js
@@ -1,4 +1,4 @@
-export default async function fetchAvailablePlaces() {
+export async function fetchAvailablePlaces() {
   const response = await fetch("http://localhost:3000/places");
   const resData = await response.json();
 
@@ -7,4 +7,22 @@ export default async function fetchAvailablePlaces() {
   }
 
   return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places: places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(resData.message || "Failed to update user places");
+  }
+
+  return resData.message;
 }
