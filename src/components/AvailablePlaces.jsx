2014c86445433ac679789fe3871302ff460b5722
WodaMineralna <raknieborak65@gmail.com>
2025-04-16 20:45:50 +0200

    minor fix

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import ErrorPage from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
-import fetchAvailablePlaces from "../http.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
